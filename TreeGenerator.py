ab81d8c95663fa37b49178bbc42cd49eeeecb46e
etking <94794807+etKing666@users.noreply.github.com>
2023-04-05 02:11:38 +0200

    Calculation part is done. Working on alaysis now.

--- TreeGenerator.py
+++ TreeGenerator.py
@@ -14,9 +14,9 @@ for i in range(length):
     name = data[leaf]['name']
     parent = data[leaf]['parent']
     if parent == "None":
-        parent=None #For root node, the parent value is set to "None" as per the anytree documentation
+        parent = None  # For root node, the parent value is set to "None" as per the anytree documentation
     elif parent == "root":
-        parent=root_node
+        parent = root_node
     else:
         for x in nodes.keys():
             if parent == x:
@@ -24,15 +24,68 @@ for i in range(length):
             else:
                 pass
 
-    probability = data[leaf]['probability']
-    print(probability)
-    leaf = Node(name=name, parent=parent, probability=probability)
+    value = data[leaf]['value']
+    if value is None:
+        value = 0.0  # In order to have a numeric value
+    else:
+        value = float(value)  # In order to prevent possible errors in user input (e.g. integer values)
 
-    if leaf.parent == None:
+    leaf = Node(name=name, parent=parent, value=value)
+
+    if leaf.parent is None:
         root_node = leaf
         nodes["root"] = root_node
     else:
         nodes[leaf.name] = leaf
 
-print(RenderTree(root_node))
-DotExporter(root_node).to_picture("root.png")
+def generate():
+    """Generates the tree on the console as well as exports a visual diagram to the working folder"""
+    print(RenderTree(root_node))
+    DotExporter(root_node).to_picture("root.png")
+    return
+
+def calculate():
+    height = root_node.height  # Calculating total height of the tree
+    reqDepth = height - 1  # To reach the first parents, required depth is height-1
+
+    # Iterative value calculation:
+    while reqDepth >= 0:
+        for node in nodes.values():
+            if node.children:
+                if node.depth == reqDepth:
+                    children = []
+                    children.extend(node.children)
+                    numChild = len(children)  # The number of children the parent node has
+                    if numChild == 0:
+                        numChild = 1  # In order escape the division by zero error for leaves with no children
+                    total = 0
+                    for child in children:
+                        total += child.value
+                    node.value = total / numChild
+
+        reqDepth -= 1
+
+    print("""=================
+    THE FINAL TREE:
+    ==============""")
+    print(RenderTree(root_node))
+    print(f"The overall value is: {root_node.value}")
+    return
+
+def analyse():
+    print("""From this section, you can enter different values to the nodes to analyse the possible impact of potential
+    threats to the business. The values suggested by Microsoft are:
+    You can also enter DREAD values. 
+    Remember:
+    1. You can enter a value to a parent node. In this case, the value calculated using the children nodes will be overriden.
+    2. The algorithm uses a simple average. Keeping this in mind, you can enter any value (be it potential damage, cost 
+    or any other value). However, it doesn't make probabilistic (OR/AND) calculations. 
+    ================================
+    THE CURRENT STATE OF THE TREE:
+    =========================""")
+    print(RenderTree(root_node))
+    nodeSelect  = input("""Please enter the node that you want to enter a value. HELP: If the node is:"
+                        '/Business/Threat_Category/Threat_Name' you should enter Threat_Name to select the node.""")
+    while True:
+        pass
+        return False
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from cmd import Cmd
-import TreeGenerator
+from TreeGenerator import generate, calculate
+
 
 class Prompt(Cmd):
+
     def do_generate(self, inp):
         """Generates the attack tree based on the .json file in the working directory."""
-        listdir()
+        generate()
         return
 
-    def do_add(self, inp):
-        """Adds two numbers together and provides the result. Example usage: add 2 4 (space is required between the numbers)"""
-        num1 = int(inp[0])  # We typecast the input because the arguments are taken as strings.
-        num2 = int(inp[2])  # inp[0] is the first number, inp[1] a space and inp[2] is the second number.
-        result = num1 + num2
-        print(f"Result: {result}")
+    def do_calculate(self, inp):
+        """Calculates the overall risk rating based on the probability values of the leaves."""
+        calculate()
         return
 
     def do_exit(self, inp):
         """Exists the shell"""
         print("Thanks for using the shell. Bye!")
         return True  # Returns true so that Cmd.postcmd() hook method can terminate the execution.
+
 
 Prompt().cmdloop()
